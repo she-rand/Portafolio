c748365913c366bc47a8f2d047fad3ca487743d0
she-rand <liliana.zurita@gmail.com>
2019-09-05 21:48:45 -0400

    first commit portafolio

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+   $("a").click(function(event){
+   if(this.hash!==""){
+     event.preventDefault();
+     var gato=this.hash;
+     $("html,body").animate({
+       scrollTop: $(gato).offset().top
+     },800, function(){
+       window.location.hash=gato;
+     });
+   }
+    });
+$('[data-toggle="tooltip"]').tooltip();
+
+});
